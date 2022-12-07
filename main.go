55133f40b066dfbebed3caa45e353f99333a101e
Lerking <jan.lerking@gmail.com>
2022-12-07 22:12:37 +0100

    0.0.1 Initial checkin. /JL

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+type MyMainWindow struct {
+	*walk.MainWindow
+	te *walk.TextEdit
+}
+
+const (
+	AppName   string = "BeadImager"
+	Version   string = "0.0.1"
+	CopyRight string = "©2022 Jan Lerking"
+	STD_MESS  string = "Ready"
+)
+
+func main() {
+	mw := &MyMainWindow{}
+
+	if _, err := (MainWindow{
+		AssignTo: &mw.MainWindow,
+		Title:    AppName + " " + Version,
+		MinSize:  Size{800, 600},
+
+		Layout: VBox{MarginsZero: true},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					PushButton{
+						Text: "Edit Animal",
+					},
+					TextEdit{
+						AssignTo: &mw.te,
+						ReadOnly: true,
+					},
+				},
+			},
+		},
+	}.Run()); err != nil {
+		log.Fatal(err)
+	}
+}
